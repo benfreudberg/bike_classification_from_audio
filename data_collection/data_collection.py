f66de66727a9fd094217948fd745e529a4541c2e
ben_freudberg <ben.freudberg@gmail.com>
2024-02-14 13:23:37 -0800

    make vars protected

--- data_collection/data_collection.py
+++ data_collection/data_collection.py
@@ -47,68 +47,68 @@ class EventSampleRecorder:
     __DEFAULT_LOCATION_STRING = "no location set"
 
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("1000x500")
-        self.root.title("Audio Sample Recording Application")
+        self.__root = tk.Tk()
+        self.__root.geometry("1000x500")
+        self.__root.title("Audio Sample Recording Application")
 
-        self.entry_frame = tk.Frame(self.root)
-        self.entry_frame.columnconfigure(0, weight=1)
-        self.entry_frame.columnconfigure(1, weight=1)
+        self.__entry_frame = tk.Frame(self.__root)
+        self.__entry_frame.columnconfigure(0, weight=1)
+        self.__entry_frame.columnconfigure(1, weight=1)
 
-        self.set_output_directory_button = (
-            tk.Button(self.entry_frame,
+        self.__set_output_directory_button = (
+            tk.Button(self.__entry_frame,
                       text="Set Output Directory",
                       command=self.__set_output_directory))
-        self.set_output_directory_button.grid(row=0, column=0,
+        self.__set_output_directory_button.grid(row=0, column=0,
                                               sticky=tk.W+tk.E)
-        self.directory_text_box = tk.Entry(self.entry_frame, width=100)
-        self.directory_text_box.insert(0, self.__DEFAULT_DIRECTORY_STRING)
-        self.directory_text_box.grid(row=0, column=1,
+        self.__directory_text_box = tk.Entry(self.__entry_frame, width=100)
+        self.__directory_text_box.insert(0, self.__DEFAULT_DIRECTORY_STRING)
+        self.__directory_text_box.grid(row=0, column=1,
                                      sticky=tk.W+tk.E, pady=10)
 
-        self.location_label = tk.Label(self.entry_frame,
+        self.__location_label = tk.Label(self.__entry_frame,
                                        text="Sample Location")
-        self.location_label.grid(row=1, column=0, sticky=tk.W+tk.E, pady=10)
-        self.location_text_box = tk.Entry(self.entry_frame)
-        self.location_text_box.insert(0, self.__DEFAULT_LOCATION_STRING)
-        self.location_text_box.grid(row=1, column=1, sticky=tk.W+tk.E, pady=10)
+        self.__location_label.grid(row=1, column=0, sticky=tk.W+tk.E, pady=10)
+        self.__location_text_box = tk.Entry(self.__entry_frame)
+        self.__location_text_box.insert(0, self.__DEFAULT_LOCATION_STRING)
+        self.__location_text_box.grid(row=1, column=1, sticky=tk.W+tk.E, pady=10)
 
-        self.entry_frame.pack(pady=50)
+        self.__entry_frame.pack(pady=50)
 
-        self.button_frame = tk.Frame(self.root)
-        self.button_frame.columnconfigure(0, weight=1)
-        self.button_frame.columnconfigure(1, weight=1)
+        self.__button_frame = tk.Frame(self.__root)
+        self.__button_frame.columnconfigure(0, weight=1)
+        self.__button_frame.columnconfigure(1, weight=1)
 
-        self.button_bike = tk.Button(self.button_frame,
+        self.__button_bike = tk.Button(self.__button_frame,
                                      text="Record \"bike\"",
                                      font=('Arial', 32),
                                      command=self.__bike_click_handler)
-        self.button_bike.grid(row=0, column=0, sticky=tk.W+tk.E)
-        self.button_not_bike = tk.Button(self.button_frame,
+        self.__button_bike.grid(row=0, column=0, sticky=tk.W+tk.E)
+        self.__button_not_bike = tk.Button(self.__button_frame,
                                          text="Record \"notbike\"",
                                          font=('Arial', 32),
                                          command=self.__not_bike_click_handler)
-        self.button_not_bike.grid(row=0, column=1, sticky=tk.W+tk.E)
+        self.__button_not_bike.grid(row=0, column=1, sticky=tk.W+tk.E)
 
-        self.button_frame.pack(pady=50)
+        self.__button_frame.pack(pady=50)
 
-        self.root.mainloop()
+        self.__root.mainloop()
 
     def __click_handler(self, bike):
-        if self.directory_text_box.get() == self.__DEFAULT_DIRECTORY_STRING:
+        if self.__directory_text_box.get() == self.__DEFAULT_DIRECTORY_STRING:
             print('No output directory set')
             return
         bike_string = "bike" if bike else "notbike"
         timestamp = datetime.datetime.now()
         timestamp_string = timestamp.strftime("%Y-%m-%d-%H%M%S")
-        location_string = self.location_text_box.get()
+        location_string = self.__location_text_box.get()
         if location_string == self.__DEFAULT_LOCATION_STRING:
             print('No location set')
             return
         file_name = (timestamp_string + "_" +
                      location_string + "_" +
                      bike_string + ".wav")
-        directory = os.path.join(self.directory_text_box.get(), file_name)
+        directory = os.path.join(self.__directory_text_box.get(), file_name)
         record_sample(directory)
 
     def __bike_click_handler(self):
@@ -121,5 +121,5 @@ class EventSampleRecorder:
         directory = askdirectory(title="Set Output Directory",
                                  initialdir=os.getcwd())
         directory = os.path.normpath(directory)
-        self.directory_text_box.delete(0, tk.END)
-        self.directory_text_box.insert(0, directory)
+        self.__directory_text_box.delete(0, tk.END)
+        self.__directory_text_box.insert(0, directory)
